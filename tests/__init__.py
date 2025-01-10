dfeeadeddaa28b620c77906f786bff229a5e8c24
Ali YARO <aliyaro104@gmail.com>
2025-01-10 17:59:54 +0100

    Replaced 'constify' with 'constify/src/constify' in the comment

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 import sys
 from pathlib import Path
 
-# Adds 'src' to PYTHONPATH for importing the 'constify' package
+# Adds 'src' to PYTHONPATH for importing the 'constify/src/constify' package
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'src'))
