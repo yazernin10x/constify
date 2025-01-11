4c0644fe8fe49f2a472283ac1000dfc730ce47d9
Ali YARO <aliyaro104@gmail.com>
2025-01-11 18:20:10 +0100

    Added a docstring

--- src/constify/_freezeparams.py
+++ src/constify/_freezeparams.py
@@ -1,3 +1,10 @@
+"""Define functions to secure the parameters of functions.
+
+Functions
+---------
+* freezeparams(func) - Secure mutable parameters from modifications.
+"""
+
 import copy
 import inspect
 import functools
@@ -5,6 +12,42 @@ from typing import Any, Callable
 
 
 def freezeparams(func: Callable[..., Any]) -> Callable[..., Any]:
+    """Secure mutable parameters from modifications.
+
+    At each call of the decorated function `func`, it ensures the integrity
+    of parameters with mutable default values and parameters receiving mutable
+    arguments, preventing any accidental modifications and isolating the calls
+    ,making the code more reliable and predictable.
+
+    Parameters
+    ----------
+    func: Callable[..., Any]
+        The function to decorate.
+
+    Returns
+    -------
+    Callable[..., Any]
+        A new function with secure mutable parameters.
+
+    Example
+    -------
+    >>> @freezeparams
+    ... def add_to(value, liste=[]):
+    ...    liste.append(value)
+    ...    return liste
+    ...
+    >>> add_to(56)
+    [56]
+    >>> add_to(value=98)
+    [98]
+    >>> my_list = [1, 2]
+    >>> add_to(3, my_list)
+    [1, 2, 3]
+    >>> my_list
+    [1, 2]
+    >>>
+    """
+
     @functools.wraps(func)
     def wrapper(*args: tuple[Any], **kwargs: dict[str, Any]) -> Any:
         parameters = inspect.signature(func).parameters
