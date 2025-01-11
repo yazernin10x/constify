461d6c638ce8aa434afa854988a4b048fb8f6618
Ali YARO <aliyaro104@gmail.com>
2025-01-11 17:30:24 +0100

    Type annotation added to add_to

--- tests/src/constify/test_freezeparams.py
+++ tests/src/constify/test_freezeparams.py
@@ -1,17 +1,19 @@
 import unittest
+from typing import Any
 
 from constify import freezeparams
 
+
 class TestFreezeParams(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         @freezeparams
-        def add_to(value, liste=[]):
+        def add_to(value: Any, liste: list[Any] = []) -> list[Any]:
             liste.append(value)
             return liste
 
         self.add_to = add_to
 
-    def test_default_value(self):
+    def test_default_value(self) -> None:
         test_cases = [(56, [56]), (98, [98])]
 
         for value, expected_value in test_cases:
@@ -19,7 +21,7 @@ class TestFreezeParams(unittest.TestCase):
                 self.assertEqual(self.add_to(value), expected_value)
                 self.assertEqual(self.add_to(value=value), expected_value)
 
-    def test_non_default_value(self):
+    def test_non_default_value(self) -> None:
         value = 3
         my_list = [1, 2]
         expected_value = [1, 2, 3]
