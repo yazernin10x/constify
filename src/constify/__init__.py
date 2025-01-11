b08b735cc3aa076230011f870edfcf6e03c70689
Ali YARO <aliyaro104@gmail.com>
2025-01-11 17:22:18 +0100

    Added __all__ to restrict imports

--- src/constify/__init__.py
+++ src/constify/__init__.py
@@ -1,5 +1,7 @@
 from ._freezeparams import freezeparams
 
+__all__ = ["freezeparams"]
+
 # Adjust __module__ to show objects as part of the current package
 for value in list(locals().values()):
     if getattr(value, "__module__", "").startswith(__name__):
